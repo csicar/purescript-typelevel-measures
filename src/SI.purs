9d3c28ca15ee7547db150eee991122950480f331
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-07-31 16:53:43 +0200

    reformat purty

--- src/SI.purs
+++ src/SI.purs
@@ -1,17 +1,19 @@
 module Type.Data.Units.SI where
 
-
 import Prelude
-
 import Type.Data.Units (class ShowMeasure, type (*), type (:), MeasureExp, Measured(..), kind Measure)
 import Type.Data.Peano.Int (N1, N2, N3, P1, P2, N4)
 
 -- Meter
 foreign import data MeterT :: Measure
-type Meter exp r = (meter :: MeasureExp MeterT exp | r)
-type Meter' r = Meter P1 r
 
-meter :: ∀a. Semiring a => a : Meter P1 ()
+type Meter exp r
+  = ( meter :: MeasureExp MeterT exp | r )
+
+type Meter' r
+  = Meter P1 r
+
+meter :: ∀ a. Semiring a => a : Meter P1 ()
 meter = Measured one
 
 instance showMeter :: ShowMeasure MeterT where
@@ -19,32 +21,41 @@ instance showMeter :: ShowMeasure MeterT where
 
 -- Kilogram
 foreign import data KgT :: Measure
-type Kg exp r = (kg :: MeasureExp KgT exp | r)
-type Kg' r = Kg P1 r
 
-kg :: ∀a. Semiring a => a : Kg P1 ()
+type Kg exp r
+  = ( kg :: MeasureExp KgT exp | r )
+
+type Kg' r
+  = Kg P1 r
+
+kg :: ∀ a. Semiring a => a : Kg P1 ()
 kg = Measured one
 
 instance showkg :: ShowMeasure KgT where
   showMeasure _ = "kg"
 
-
 -- Second
 foreign import data SecT :: Measure
-type Sec exp r = (sec :: MeasureExp SecT exp | r)
-type Sec' r = Sec P1 r
 
-sec :: ∀a. Semiring a => a : Sec P1 ()
+type Sec exp r
+  = ( sec :: MeasureExp SecT exp | r )
+
+type Sec' r
+  = Sec P1 r
+
+sec :: ∀ a. Semiring a => a : Sec P1 ()
 sec = Measured one
 
 instance showSec :: ShowMeasure SecT where
-   showMeasure _ = "s"
+  showMeasure _ = "s"
 
 -- Ampere
 foreign import data AmpereT :: Measure
-type Ampere exp r = (ampere :: MeasureExp AmpereT exp | r)
 
-ampere :: ∀a. Semiring a => a : Ampere P1 ()
+type Ampere exp r
+  = ( ampere :: MeasureExp AmpereT exp | r )
+
+ampere :: ∀ a. Semiring a => a : Ampere P1 ()
 ampere = Measured one
 
 instance showAmpere :: ShowMeasure AmpereT where
@@ -52,10 +63,14 @@ instance showAmpere :: ShowMeasure AmpereT where
 
 -- Mole
 foreign import data MoleT :: Measure
-type Mole exp r = (mole :: MeasureExp MoleT exp | r)
-type Mole' r = Mole P1 r
 
-mole :: ∀a. Semiring a => a : Mole P1 ()
+type Mole exp r
+  = ( mole :: MeasureExp MoleT exp | r )
+
+type Mole' r
+  = Mole P1 r
+
+mole :: ∀ a. Semiring a => a : Mole P1 ()
 mole = Measured one
 
 instance showMole :: ShowMeasure MoleT where
@@ -63,70 +78,85 @@ instance showMole :: ShowMeasure MoleT where
 
 -- Kelvin
 foreign import data KelvinT :: Measure
-type Kelvin exp r = (kelvin :: MeasureExp KelvinT exp | r)
-type Kelvin' r = Kelvin P1 r
 
-kelvin :: ∀a. Semiring a => a : Kelvin P1 ()
+type Kelvin exp r
+  = ( kelvin :: MeasureExp KelvinT exp | r )
+
+type Kelvin' r
+  = Kelvin P1 r
+
+kelvin :: ∀ a. Semiring a => a : Kelvin P1 ()
 kelvin = Measured one
 
 instance showKelvin :: ShowMeasure KelvinT where
   showMeasure _ = "K"
 
-
 -- Candela
 foreign import data CandelaT :: Measure
-type Candela exp r = (candela :: MeasureExp CandelaT exp | r)
-type Candela' r = Candela P1 r
 
-candela :: ∀a. Semiring a => a : Candela P1 ()
+type Candela exp r
+  = ( candela :: MeasureExp CandelaT exp | r )
+
+type Candela' r
+  = Candela P1 r
+
+candela :: ∀ a. Semiring a => a : Candela P1 ()
 candela = Measured one
 
 instance showCandela :: ShowMeasure CandelaT where
   showMeasure _ = "cd"
 
 -- Derived
+type Newton r
+  = Kg P1 * Meter P1 * Sec N2 * r
 
-type Newton r = Kg P1 * Meter P1 * Sec N2 * r
-
-newton :: ∀a. Semiring a => a : Newton ()
+newton :: ∀ a. Semiring a => a : Newton ()
 newton = Measured one
 
-type Hertz r = Sec N1 * r
+type Hertz r
+  = Sec N1 * r
 
-hertz :: ∀a. Semiring a => a : Hertz ()
+hertz :: ∀ a. Semiring a => a : Hertz ()
 hertz = Measured one
 
-type Pascal r = Kg P1 * Meter N1 * Sec N2 * r
+type Pascal r
+  = Kg P1 * Meter N1 * Sec N2 * r
 
-pascal :: ∀a. Semiring a => a : Pascal ()
+pascal :: ∀ a. Semiring a => a : Pascal ()
 pascal = Measured one
 
-type Joule r = Kg P1 * Meter P2 * Sec N2 * r
+type Joule r
+  = Kg P1 * Meter P2 * Sec N2 * r
 
-joule :: ∀a. Semiring a => a : Joule ()
+joule :: ∀ a. Semiring a => a : Joule ()
 joule = Measured one
 
-type Watt r = Kg P1 * Meter P2 * Sec N3 * r
+type Watt r
+  = Kg P1 * Meter P2 * Sec N3 * r
 
-watt :: ∀a. Semiring a => a : Watt ()
+watt :: ∀ a. Semiring a => a : Watt ()
 watt = Measured one
 
-type Coulomb r = Ampere P1 * Sec P1 * r
+type Coulomb r
+  = Ampere P1 * Sec P1 * r
 
-coulomb :: ∀a. Semiring a => a : Coulomb ()
+coulomb :: ∀ a. Semiring a => a : Coulomb ()
 coulomb = Measured one
 
-type Volt r = Kg P1 * Meter P2 * Ampere N1 * Sec N3 * r
+type Volt r
+  = Kg P1 * Meter P2 * Ampere N1 * Sec N3 * r
 
-volt :: ∀a. Semiring a => a : Volt ()
+volt :: ∀ a. Semiring a => a : Volt ()
 volt = Measured one
 
-type Farad r = Ampere P2 * Sec N4 * Kg N2 * Meter N2 * r
+type Farad r
+  = Ampere P2 * Sec N4 * Kg N2 * Meter N2 * r
 
-farad :: ∀a. Semiring a => a : Farad ()
+farad :: ∀ a. Semiring a => a : Farad ()
 farad = Measured one
 
-type Ohm r = Kg P1 * Meter P2 * Sec N3 * Ampere N2 * r
+type Ohm r
+  = Kg P1 * Meter P2 * Sec N3 * Ampere N2 * r
 
-ohm :: ∀a. Semiring a => a : Ohm ()
+ohm :: ∀ a. Semiring a => a : Ohm ()
 ohm = Measured one
--- src/Units.purs
+++ src/Units.purs
@@ -2,7 +2,6 @@ module Type.Data.Units where
 
 import Prelude
 import Prelude as Prelude
-
 import Data.Array (index)
 import Data.Maybe (Maybe(..))
 import Prim.RowList (kind RowList, Cons, Nil)
@@ -11,12 +10,11 @@ import Type.Prelude (class ListToRow, class RowToList, class Union, RLProxy, RPr
 import Type.Row (RowApply)
 import Unsafe.Coerce (unsafeCoerce)
 
-
-undefined :: ∀a. a
+undefined :: ∀ a. a
 undefined = unsafeCoerce unit
 
-
-data Measured v (u :: # Type) = Measured v
+data Measured v (u :: #Type)
+  = Measured v
 
 instance eqMeasured :: (Eq v) => Eq (Measured v u) where
   eq (Measured a) (Measured b) = eq a b
@@ -25,34 +23,28 @@ instance ordMeasured :: (Ord v) => Ord (Measured v u) where
   compare (Measured a) (Measured b) = compare a b
 
 foreign import kind Measure
+
 data MProxy (m :: Measure)
+
 data MeasureExp (m :: Measure) (exp :: Int)
-
-
 
 -- combines two sequential same symbols by adding their values
 class Combine (original :: RowList) (combined :: RowList) | original -> combined
 
-
 instance combineSame ∷ (SumInt exp1 exp2 exp3, Combine tail tail') => Combine (Cons sym (MeasureExp m exp1) (Cons sym (MeasureExp m exp2) tail)) (Cons sym (MeasureExp m exp3) tail')
-else
-instance combineDifferent :: (Combine rest rest') => Combine (Cons sym1 (MeasureExp m1 exp1) rest) (Cons sym1 (MeasureExp m1 exp1) rest')
+else instance combineDifferent :: (Combine rest rest') => Combine (Cons sym1 (MeasureExp m1 exp1) rest) (Cons sym1 (MeasureExp m1 exp1) rest')
 
 instance combineNil :: Combine Nil Nil
 
-
-combine :: ∀a b. (Combine a b) => RLProxy a -> RLProxy b
+combine :: ∀ a b. (Combine a b) => RLProxy a -> RLProxy b
 combine = undefined
 
-
-
-class AddRows (a :: # Type) (b :: # Type) (sum :: #Type) | a b -> sum
+class AddRows (a :: #Type) (b :: #Type) (sum :: #Type) | a b -> sum
 
 instance addRowsCombine :: (Union a b sum, RowToList sum rsum, Combine rsum resultList, ListToRow resultList result) => AddRows a b result
 
-addRows :: ∀a b sum. (AddRows a b sum) => RProxy a -> RProxy b -> RProxy sum
+addRows :: ∀ a b sum. (AddRows a b sum) => RProxy a -> RProxy b -> RProxy sum
 addRows a b = undefined
-
 
 class InverseRowList (original :: RowList) (inverted :: RowList) | original -> inverted, inverted -> original
 
@@ -60,44 +52,41 @@ instance inverseNil ∷ InverseRowList Nil Nil
 
 instance inverseCons :: (InverseRowList tail inverseTail, Inverse ty inverseTy) => InverseRowList (Cons sym (MeasureExp m ty) tail) (Cons sym (MeasureExp m inverseTy) inverseTail)
 
+class InverseRow (original :: #Type) (inverted :: #Type) | inverted -> original
 
-class InverseRow (original :: # Type) (inverted :: # Type) | inverted -> original
-
-instance inverseRowList ∷ (RowToList original originalList, InverseRowList originalList resultList, ListToRow resultList result) =>  InverseRow original result
+instance inverseRowList ∷ (RowToList original originalList, InverseRowList originalList resultList, ListToRow resultList result) => InverseRow original result
 
 -- Methods
-
-mult :: ∀a b c v. AddRows a b c => Semiring v => Measured v a -> Measured v b -> Measured v c
+mult :: ∀ a b c v. AddRows a b c => Semiring v => Measured v a -> Measured v b -> Measured v c
 mult (Measured a) (Measured b) = Measured (a `Prelude.mul` b)
 
 infixl 7 mult as **
 
-divide :: ∀a b c v ib. InverseRow b ib => AddRows a ib c => EuclideanRing v => Measured v a -> Measured v b -> Measured v c
+divide :: ∀ a b c v ib. InverseRow b ib => AddRows a ib c => EuclideanRing v => Measured v a -> Measured v b -> Measured v c
 divide (Measured a) (Measured b) = Measured (a / b)
 
 infixl 7 divide as //
 
-liftV :: ∀a. Semiring a => a -> a : ()
+liftV :: ∀ a. Semiring a => a -> a : ()
 liftV = Measured
 
-add :: ∀a b c v. Semiring v => AddRows a b c => Measured v a -> Measured v b -> Measured v c
+add :: ∀ a b c v. Semiring v => AddRows a b c => Measured v a -> Measured v b -> Measured v c
 add (Measured a) (Measured b) = Measured (a `Prelude.add` b)
 
 infixl 6 add as ++
 
-sub :: ∀a b c v. Ring v => AddRows a b c => Measured v a -> Measured v b -> Measured v c
+sub :: ∀ a b c v. Ring v => AddRows a b c => Measured v a -> Measured v b -> Measured v c
 sub (Measured a) (Measured b) = Measured (a `Prelude.sub` b)
 
 infixl 6 sub as -|
 
-zeroV :: ∀v. Semiring v => Measured v ()
+zeroV :: ∀ v. Semiring v => Measured v ()
 zeroV = Measured Prelude.zero
 
-oneV :: ∀v. Semiring v => Measured v ()
+oneV :: ∀ v. Semiring v => Measured v ()
 oneV = Measured Prelude.one
 
 -- Syntactic Sugar
-
 infixr 4 type RowApply as *
 
 infix 3 type Measured as :
@@ -112,23 +101,26 @@ instance showRowNil :: Show (ShowRow Nil) where
   show _ = ""
 
 unicodeExponents :: Array String
-unicodeExponents = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
+unicodeExponents = [ "⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹" ]
 
 negExp :: String
 negExp = "⁻"
 
 constructExp :: Int -> String
-constructExp x | x >= 0 = constructPosExp x
+constructExp x
+  | x >= 0 = constructPosExp x
+
 constructExp x = negExp <> (constructPosExp (-x))
 
 constructPosExp :: Int -> String
 constructPosExp x = case (index unicodeExponents x) of
-   Nothing -> constructPosExp (x `div` 10) <> constructPosExp (x `mod` 10)
-   Just exp -> exp
+  Nothing -> constructPosExp (x `div` 10) <> constructPosExp (x `mod` 10)
+  Just exp -> exp
 
-instance showRowCons :: (ShowMeasure ty, Show (ShowRow tail), IsInt exp) 
-  => Show (ShowRow (Cons sym (MeasureExp ty exp) tail)) where
+instance showRowCons ::
+  (ShowMeasure ty, Show (ShowRow tail), IsInt exp) =>
+  Show (ShowRow (Cons sym (MeasureExp ty exp) tail)) where
   show _ = showMeasure (undefined :: MProxy ty) <> (constructExp $ reflectInt (undefined :: IProxy exp)) <> (show (undefined :: ShowRow tail))
 
 instance showMeasured :: (Show v, RowToList r rl, Show (ShowRow rl)) => Show (Measured v r) where
-   show (Measured v) = show v <> "·" <> show (undefined :: ShowRow rl)
+  show (Measured v) = show v <> "·" <> show (undefined :: ShowRow rl)
